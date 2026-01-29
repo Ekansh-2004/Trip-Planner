cd484a62eb7afb94da084b6e29f0d231cea72f84
Ekansh-2004 <chauhan.ekansh06@gmail.com>
2026-01-30 02:19:43 +0530

    Fixed REACT_APP to Vite and CSS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,11 @@
-// https://vite.dev/config/
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vite.dev/config/
+
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
 	define: {
 		"import.meta.env.VITE_API_URL": JSON.stringify(process.env.REACT_APP_API_URL || process.env.VITE_API_URL),
 	},
